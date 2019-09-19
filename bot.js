f40c32c25e5bb4945e5453a44c1ae5989908c7e0
Megalartem <55554255+Megalartem@users.noreply.github.com>
2019-09-19 22:05:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-BOT_TOKEN=789820897:AAFSrnNS6EAvAanF-2m4nMRmfwruB8vIyB8;
+BOT_TOKEN=789820897:'AAFSrnNS6EAvAanF-2m4nMRmfwruB8vIyB8';
 bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
